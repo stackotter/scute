a5ccc8a93f22b44efb0bc9c745b778b0e1a6405d
stackotter <stackotter@stackotter.dev>
2022-04-06 16:34:12 +1000

    Fix module syntax extension to not allow child nodes
    The issue was that the module syntax extension could have
    children so double quotes within the module were getting
    converted to smart quotes. Italics blocks within the module
    could also be rendered to html ruining any configuration
    containing asterisks.

--- Sources/CMarkExtension/extension.c
+++ Sources/CMarkExtension/extension.c
@@ -2,83 +2,53 @@
 #include <parser.h>
 #include <render.h>
 #include <stdio.h>
+#include <houdini.h>
+#include <node.h>
 
 cmark_node_type CMARK_NODE_MODULE;
 
 static cmark_node *match(cmark_syntax_extension *self, cmark_parser *parser,
                          cmark_node *parent, unsigned char character,
                          cmark_inline_parser *inline_parser) {
-    cmark_node *res = NULL;
-    int left_flanking, right_flanking, punct_before, punct_after, delims;
-    char buffer[101];
-
     if (character != '@')
         return NULL;
 
-    delims = cmark_inline_parser_scan_delimiters(inline_parser, sizeof(buffer) - 1, '@',
-                                                 &left_flanking, &right_flanking,
-                                                 &punct_before, &punct_after);
+    if (cmark_inline_parser_in_bracket(inline_parser, false) ||
+        cmark_inline_parser_in_bracket(inline_parser, true))
+        return NULL;
 
-    memset(buffer, '@', delims);
-    buffer[delims] = 0;
+    if (parent->type != CMARK_NODE_PARAGRAPH)
+        return NULL;
 
-    res = cmark_node_new_with_mem(CMARK_NODE_TEXT, parser->mem);
-    cmark_node_set_literal(res, buffer);
-    res->start_line = res->end_line = cmark_inline_parser_get_line(inline_parser);
-    res->start_column = cmark_inline_parser_get_column(inline_parser) - delims;
+    // Get the line's remaining bytes
+    cmark_chunk *chunk = cmark_inline_parser_get_chunk(inline_parser);
+    int start_pos = cmark_inline_parser_get_offset(inline_parser);
+    uint8_t *data = chunk->data + start_pos;
+    size_t size = chunk->len - start_pos;
 
-    if ((left_flanking || right_flanking) && delims == 1) {
-        cmark_inline_parser_push_delimiter(inline_parser, character, left_flanking,
-                                           right_flanking, res);
-    }
-
-    puts(buffer);
-
-    return res;
-}
-
-static delimiter *insert(cmark_syntax_extension *self, cmark_parser *parser,
-                         cmark_inline_parser *inline_parser, delimiter *opener,
-                         delimiter *closer) {
-    cmark_node *module;
-    cmark_node *tmp, *next;
-    delimiter *delim, *tmp_delim;
-    delimiter *res = closer->next;
-
-    module = opener->inl_text;
-
-    if (opener->inl_text->as.literal.len != closer->inl_text->as.literal.len)
-        goto done;
-
-    if (!cmark_node_set_type(module, CMARK_NODE_MODULE))
-        goto done;
-
-    cmark_node_set_syntax_extension(module, self);
-
-    tmp = cmark_node_next(opener->inl_text);
-
-    while (tmp) {
-        if (tmp == closer->inl_text)
+    // Parse up to the next `@`
+    size_t content_length = 0;
+    for (int i = 1; i < size; i++) {
+        if (data[i] == '@') {
+            content_length = i - 1;
             break;
-        next = cmark_node_next(tmp);
-        cmark_node_append_child(module, tmp);
-        tmp = next;
+        }
     }
 
-    module->end_column = closer->inl_text->start_column + closer->inl_text->as.literal.len - 1;
-    cmark_node_free(closer->inl_text);
+    // If no `@` was found, return null
+    if (content_length == 0)
+        return NULL;
 
-    delim = closer;
-    while (delim != NULL && delim != opener) {
-        tmp_delim = delim->previous;
-        cmark_inline_parser_remove_delimiter(inline_parser, delim);
-        delim = tmp_delim;
-    }
+    // Advance the parser over what we just parsed
+    cmark_inline_parser_set_offset(inline_parser, start_pos + content_length + 2);
 
-    cmark_inline_parser_remove_delimiter(inline_parser, opener);
+    // Create the module node
+    cmark_node *node = cmark_node_new_with_mem(CMARK_NODE_MODULE, parser->mem);
+    cmark_chunk content = cmark_chunk_dup(chunk, start_pos + 1, (bufsize_t)(content_length));
+    node->as.literal = content;
+    cmark_node_set_syntax_extension(node, self);
 
-done:
-    return res;
+    return node;
 }
 
 static const char *get_type_string(cmark_syntax_extension *extension,
@@ -86,29 +56,19 @@ static const char *get_type_string(cmark_syntax_extension *extension,
     return node->type == CMARK_NODE_MODULE ? "module" : "<unknown>";
 }
 
-static int can_contain(cmark_syntax_extension *extension, cmark_node *node,
-                       cmark_node_type child_type) {
-    if (node->type != CMARK_NODE_MODULE)
-        return false;
-
-    return CMARK_NODE_TYPE_INLINE_P(child_type);
+static void escape_html(cmark_strbuf *dest, const unsigned char *source,
+                        bufsize_t length) {
+    houdini_escape_html0(dest, source, length, 0);
 }
 
 static void html_render(cmark_syntax_extension *extension,
                         cmark_html_renderer *renderer, cmark_node *node,
                         cmark_event_type ev_type, int options) {
-    bool entering = (ev_type == CMARK_EVENT_ENTER);
-    if (entering) {
-        cmark_strbuf_puts(renderer->html, "<module>");
-    } else {
-        cmark_strbuf_puts(renderer->html, "</module>");
-    }
-}
-
-static void plaintext_render(cmark_syntax_extension *extension,
-                             cmark_renderer *renderer, cmark_node *node,
-                             cmark_event_type ev_type, int options) {
-    renderer->out(renderer, node, "@", false, LITERAL);
+    if (ev_type != CMARK_EVENT_ENTER)
+        return;
+    cmark_strbuf_puts(renderer->html, "<module>");
+    escape_html(renderer->html, node->as.literal.data, node->as.literal.len);
+    cmark_strbuf_puts(renderer->html, "</module>");
 }
 
 cmark_syntax_extension *create_module_syntax_extension(void) {
@@ -116,19 +76,14 @@ cmark_syntax_extension *create_module_syntax_extension(void) {
     cmark_llist *special_chars = NULL;
 
     cmark_syntax_extension_set_get_type_string_func(ext, get_type_string);
-    cmark_syntax_extension_set_can_contain_func(ext, can_contain);
     cmark_syntax_extension_set_html_render_func(ext, html_render);
-    cmark_syntax_extension_set_plaintext_render_func(ext, plaintext_render);
     CMARK_NODE_MODULE = cmark_syntax_extension_add_node(1);
 
     cmark_syntax_extension_set_match_inline_func(ext, match);
-    cmark_syntax_extension_set_inline_from_delim_func(ext, insert);
 
     cmark_mem *mem = cmark_get_default_mem_allocator();
     special_chars = cmark_llist_append(mem, special_chars, (void *)'@');
     cmark_syntax_extension_set_special_inline_chars(ext, special_chars);
-
-    cmark_syntax_extension_set_emphasis(ext, 1);
 
     return ext;
 }
