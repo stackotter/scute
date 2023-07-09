fd52fa7186ed22ab2b8d941b0db08489762c393d
stackotter <stackotter@stackotter.dev>
2023-07-09 16:32:39 +1000

    Add version to CLI

--- Sources/scute/ScuteCLI.swift
+++ Sources/scute/ScuteCLI.swift
@@ -5,6 +5,7 @@ import ScuteCore
 struct ScuteCLI: AsyncParsableCommand {
     static var configuration = CommandConfiguration(
         abstract: "A Site Creation (u) Tool (e).",
+        version: "0.1.0",
         subcommands: [PreviewCommand.self, BuildCommand.self, CreateCommand.self],
         defaultSubcommand: PreviewCommand.self
     )
